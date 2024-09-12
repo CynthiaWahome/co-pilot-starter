6d398daefc3676b5649b5613ca4e18732f1e597d
cynthia <cynthiaawsajira@gmail.com>
2024-09-12 23:53:26 +0300

    [fix] fixed a bug, add route

--- app.py
+++ app.py
@@ -10,4 +10,27 @@ def seconds_since_epoch():
 @app.route("/")
 def seconds():
     return seconds_since_epoch()
+import time
+from flask import Flask
+
+app = Flask(__name__)
+
+def seconds_since_epoch():
+    epoch = time.time()
+    return str(epoch)
+
+def hours_since_epoch():
+    epoch = time.time()
+    return int(epoch // 3600)
+
+@app.route("/")
+def seconds():
+    return str(seconds_since_epoch())
+
+@app.route("/hours")
+def hours():
+    return str(hours_since_epoch())
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
